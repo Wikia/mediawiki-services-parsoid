e19cd5158eeb6c4bb28cf8248611392e921a9b9c
Máté Szabó <tk.999.wikia@gmail.com>
2017-11-17 01:20:13 +0100

    SUS-3260: Move parsoid config and Dockerfile to repo (#28)
    SUS-3260: Move parsoid config and Dockerfile to repo

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:6-alpine
+
+RUN mkdir -p /usr/wikia/parsoid
+
+RUN adduser -SH -g '' -h /nonexistent parsoid
+RUN chown -R parsoid /usr/wikia/parsoid
+
+RUN apk add --no-cache git python make g++
+
+# cache dependencies in a separate layer from the main app
+COPY package.json /usr/wikia/parsoid
+WORKDIR /usr/wikia/parsoid
+RUN npm install --production
+
+COPY . /usr/wikia/parsoid
+
+USER parsoid
+
+CMD ["node", "api/server.js"]
---
+++ tests/mocha/localSettings.js
@@ -0,0 +1,57 @@
+/* global require, describe, before, beforeEach, it, process, after */
+require('chai').should();
+
+const setup = require('../../api/localsettings').setup;
+
+describe('Dynamic local settings', function () {
+	var oldEnv, parsoidConfig;
+
+	before(function () {
+		oldEnv = process.env.ENV;
+	});
+
+	beforeEach(function () {
+		parsoidConfig = {};
+	});
+
+	it('does not define cache properties if env is not set', function () {
+		process.env.ENV = '';
+
+		setup(parsoidConfig);
+
+		parsoidConfig.should.not.have.property('parsoidCacheURI');
+		parsoidConfig.should.not.have.property('parsoidCacheProxy');
+		parsoidConfig.should.not.have.property('defaultAPIProxyURI');
+	});
+
+
+	var devTestCases = [ 'dev', 'DEV', 'dEV' ];
+	devTestCases.forEach(function (envName) {
+		it(`does not define cache properties if env = ${envName}`, function () {
+			process.env.ENV = envName;
+
+			setup(parsoidConfig);
+
+			parsoidConfig.should.not.have.property('parsoidCacheURI');
+			parsoidConfig.should.not.have.property('parsoidCacheProxy');
+			parsoidConfig.should.not.have.property('defaultAPIProxyURI');
+		});
+	});
+
+	var nonDevTestCases = [ 'prod', 'staging' ];
+	nonDevTestCases.forEach(function (envName) {
+		it(`defines cache properties if env = ${envName}`, function () {
+			process.env.ENV = envName;
+
+			setup(parsoidConfig);
+
+			parsoidConfig.should.have.property('parsoidCacheURI', `http://${envName}.parsoid-cache/`);
+			parsoidConfig.should.have.property('parsoidCacheProxy', `http://${envName}.icache.service.consul:80/`);
+			parsoidConfig.should.have.property('defaultAPIProxyURI', `http://${envName}.icache.service.consul:80/`);
+		});
+	});
+
+	after(function () {
+		process.env.ENV = oldEnv;
+	});
+});
