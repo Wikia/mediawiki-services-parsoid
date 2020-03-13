03a64b96d6ccc3fca43d0e200bf49770308eaa7c
Jacek 'Mech' Woźniak <wozniak.jacek@gmail.com>
2020-03-13 11:07:05 +0100

    Stop using the old app deployment
    Switch to ucp

--- api/localsettings.js
+++ api/localsettings.js
@@ -9,7 +9,7 @@ exports.setup = function( parsoidConfig ) {
 		const envName = (process.env.ENV + '').toLowerCase();
 
 		if ( envName !== '' && envName !== 'dev' ) {
-			parsoidConfig.defaultAPIProxyURI = 'http://mediawiki-prod:80/';
+			parsoidConfig.defaultAPIProxyURI = 'http://mediawiki-prod-ucp:80/';
 		} else {
 			// PLATFORM-3727: make sure not to send API calls through Fastly in dev as well
 			parsoidConfig.defaultAPIProxyURI = 'http://border.service.consul:80/';
